1bee4bc2f30fc17c4103707586e14a80b5951fa4
daniel endaylalu <endaylaludaniel@gmail.com>
2023-11-02 22:25:18 +0300

    added post to db

--- weshareback/controller/post.js
+++ weshareback/controller/post.js
@@ -1,5 +1,6 @@
 const postRouter = require("express").Router();
 const Post = require("../models/post");
+const User = require("../models/user");
 
 postRouter.get("/", async (req, res) => {
   try {
@@ -25,6 +26,36 @@ postRouter.get("/:id", async (req, res) => {
     console.log(post); //console
 
     return res.status(200).json(post);
+  } catch (error) {
+    console.log(error); //console
+
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+});
+
+postRouter.post("/", async (req, res) => {
+  try {
+    const { location, desc, catagories, userId } = req.body;
+    console.log(req.body); //console
+
+    const newPost = new Post({
+      location,
+      desc,
+      catagories,
+      user: userId,
+    });
+    const post = await newPost.save();
+    console.log(post); //console
+
+    let user = await User.findById(userId);
+    console.log(user); //console
+
+    user.posts = user.posts.concat(post.id);
+    await user.save();
+
+    return res.status(201).json(post);
   } catch (error) {
     console.log(error); //console
 

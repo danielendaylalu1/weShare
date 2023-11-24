a8be8b5d3426e5daca5638330d7a916f5db5ba86
daniel endaylalu <endaylaludaniel@gmail.com>
2023-11-25 00:33:38 +0300

    edited a all route handler

--- weshareback/index.js
+++ weshareback/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
+const path = require("path");
 
 const port = process.env.PORT;
 
 app.get("*", (req, res) => {
-  res.sendFile("./dist/index.html");
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, (req, res) => {

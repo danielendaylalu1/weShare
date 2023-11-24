63d96901a9025648360445e4a752fc2a51ccf129
daniel endaylalu <endaylaludaniel@gmail.com>
2023-11-25 00:25:26 +0300

    added a all route handler

--- weshareback/index.js
+++ weshareback/index.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const app = require("./app");
 
 const port = process.env.PORT;
+
+app.get("*", (req, res) => {
+  res.sendFile("./dist/index.html");
+});
+
 app.listen(port, (req, res) => {
   console.log(`server is running on port ${port}`);
 });
